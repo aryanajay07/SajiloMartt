5df37de21e5b1fe5f1c50e7a95f74d3412a41683
aryanajay07 <ajay.201340@ncit.edu.np>
2024-06-10 17:40:19 +0545

    error resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,30 +15,24 @@ import Profile from './pages/User/Profile.jsx';
 import AdminRoute from './pages/Admin/AdminRoute.jsx';
 import UserList from './pages/Admin/UserList.jsx';
 
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}> 
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/Register' element={<Register/>}/>
+    <Route path="/" element={<App />}>
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
 
-    <Route path='/' element={<PrivateRoute/>}>
-          <Route path='/Profile' element={<Profile/>}/>
+      <Route path="/" element={<PrivateRoute />}>
+        <Route path="profile" element={<Profile />} />
+      </Route>
+
+      {/* AdminRoute */}
+      <Route path="admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+      </Route>
     </Route>
-
-{/* AdminRoute */}
-
-<Route path='/admin' element={<AdminRoute/>}>
-  <Route path='/userlist' element={<UserList/>}/>
-</Route>
-
-   
-    
-    </Route>
-
-
-
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
