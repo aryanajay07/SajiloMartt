e432a7b18fa3cb2de27dacf5c0e12018684a2cab
Suman Sapkota <sumansapkota771@gmail.com>
2024-06-10 15:27:08 +0545

    login and register for user and admin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,13 +10,33 @@ import store from './redux/store.js';
 //
 import Login from './pages/Auth/login.jsx';
 import Register from './pages/Auth/Register.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx';
+import Profile from './pages/User/Profile.jsx';
+import AdminRoute from './pages/Admin/AdminRoute.jsx';
+import UserList from './pages/Admin/UserList.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}> 
     <Route path='/login' element={<Login/>}/>
     <Route path='/Register' element={<Register/>}/>
+
+    <Route path='/' element={<PrivateRoute/>}>
+          <Route path='/Profile' element={<Profile/>}/>
     </Route>
+
+{/* AdminRoute */}
+
+<Route path='/admin' element={<AdminRoute/>}>
+  <Route path='/userlist' element={<UserList/>}/>
+</Route>
+
+   
+    
+    </Route>
+
+
+
   )
 )
 
